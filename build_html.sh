11b600a22a567e289d417eac3494fdbd34cec7a4
Hanif Birgani <taghipour.hanif@gmail.com>
2021-06-21 20:32:35 +0430

    separate update po file and make html scripts

---
+++ build_html.sh
@@ -0,0 +1,2 @@
+msgfmt ./locale/fa/LC_MESSAGES/index.po -o ./locale/fa/LC_MESSAGES/index.mo
+make html
--- update_po_file.sh
+++ update_po_file.sh
@@ -1,4 +1,3 @@
 curl -s https://raw.githubusercontent.com/python/peps/master/pep-0008.txt > _pep8.rst
 make gettext
 sphinx-intl update -p _build/gettext/ -l fa
-make html
